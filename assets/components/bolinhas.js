9e3ff2a3c34dc3e02a71099e63daca97f3c8b207
Luan Garcia <luanjsgarcia691@gmail.com>
2023-07-18 11:50:12 -0300

    bolinhas

--- assets/components/bolinhas.js
+++ assets/components/bolinhas.js
@@ -1,6 +1,13 @@
-import { LitElement, css } from "lit";
+import { LitElement, css, html } from "lit";
 
 export class Bolinhas extends LitElement {
+
+  static get properties() {
+    return {
+      formato: { type: String },
+    };
+  }
+
   static styles = [
     css`
       :host {
@@ -8,18 +15,53 @@ export class Bolinhas extends LitElement {
         width: 100px;
         height: 100px;
 
-        background-color: black;
+      }
+
+      .inteira {
         -webkit-mask: url("bolinhas.svg") no-repeat center;
         mask: url("bolinhas.svg") no-repeat center;
+      }
+
+      .fila-Preenchida {
+        -webkit-mask: url("bolinhas-Preenchida.svg") no-repeat center;
+        mask: url("bolinhas-Preenchida.svg") no-repeat center;
+      }
+
+      .fila-Vazia {
+        -webkit-mask: url("bolinhas-Vazia.svg") no-repeat center;
+        mask: url("bolinhas-Vazia.svg") no-repeat center;
+      }
+
+
+      .bolinhas {
+        width: 100%;
+        height: 100%;
 
         -webkit-mask-size: 100%;
         mask-size: 100%;
+        background-color: var(--cor-bolinhas, black);
       }
     `,
   ];
+/**
+   * Define o formato das bolinhas
+   * @returns Classe do formato
+   */
+  definirFormato() {
+    if (this.formato === "fila-Preenchida") {
+      return "fila-Preenchida";
+    }
+    if (this.formato === "fila-Vazia") {
+      return "fila-Vazia";
+    }
+    if (this.formato === "metade") {
+      return "metade";
+    }
+    return "inteira";
+  }
 
   render() {
-    return html``;
+    return html` <div class=${`bolinhas ${this.definirFormato()}`}></div> `;
   }
 }
 customElements.define("app-bolinhas", Bolinhas);
